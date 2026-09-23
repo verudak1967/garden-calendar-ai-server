c33ef4363677aa03564739f437028eef7a784a89
verudak1967 <verudak1967@gmail.com>
2026-09-23 11:20:44 +1100

    Fix indentation in cache return block

--- main.py
+++ main.py
@@ -1107,9 +1107,9 @@ async def ask(req: AskRequest):
         variety=req.variety or "",
         region_zone=req.region_zone or 0,
     )
-if key in server_cache:
-    metrics["cache_hits"] += 1
-    return AiResponse(text=server_cache[key], used=used, limit=limit, model=DEEPSEEK_MODEL + " (cache)")
+    if key in server_cache:
+        metrics["cache_hits"] += 1
+        return AiResponse(text=server_cache[key], used=used, limit=limit, model=DEEPSEEK_MODEL + " (cache)")
 
     system_prompt, max_tokens_for_request = build_system_prompt(req.request_type or "free")
 
